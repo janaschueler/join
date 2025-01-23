d53e2ba94e02f337666b7ea73f1937df17deaac6
Meric <meric.kalkan@outlook.de>
2025-01-23 14:52:56 +0100

    erstellt template

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * This function is used to greet someone on the comand line
+ *
+ * @param {string} name - this is the dame of the person that you want ot greet
+ */
+
+function greet(name) {
+  console("hallo" + name);
+}
