417fa2578097f2ad91d8ac4e11751297e1d63f29
Jana Schueler <janaschueler@protonmail.com>
2025-02-03 11:25:49 +0100

    up-date signup

--- scripts/db.js
+++ scripts/db.js
@@ -1,9 +1,1 @@
-let starter = [
-  {
-    dishName: "Samosa",
-    dishAmount: "(2 pieces)",
-    dishDiscription: "Pastry filled with a mixture of potatoes, green peas, cashews, and spices.",
-    dishprice: 4.99,
-    amount: 0,
-  },
-];
+let coloursArray = ["rgba(255, 122, 0, 1)", "rgba(255, 94, 179, 1)", "rgba(110, 82, 255, 1)", "rgba(147, 39, 255, 1)", "rgba(0, 190, 232, 1)", "rgba(31, 215, 193, 1)", "rgba(255, 116, 94, 1)", "rgba(255, 163, 94, 1)", "rgba(252, 113, 255, 1)", "rgba(255, 199, 1, 1)", "rgba(0, 56, 255, 1)", "rgba(195, 255, 43, 1)", "rgba(255, 230, 43, 1)", "rgba(255, 70, 70, 1)", "rgba(255, 187, 43, 1)"];
--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,7 @@
+const BASE_URL = "https://join-ab0ac-default-rtdb.europe-west1.firebasedatabase.app/";
+
+let allUser = { contactEmail: [], contactPassword: [] };
+
 function onloadFunc() {
   showLoadingMessage();
 }
@@ -18,6 +22,53 @@ function showLoadingMessage() {
   setTimeout(() => {
     loginContainer.style.display = "flex";
     footer.style.display = "block";
-    header.style.display ="flex"
+    header.style.display = "flex";
   }, 1500);
 }
+
+async function getData(path = "") {
+  let response = await fetch(BASE_URL + "signup/" + "user/" + path + ".json");
+  let responseToJson = await response.json();
+
+  let users = [];
+
+  for (let key in responseToJson) {
+    let user = responseToJson[key];
+
+    users.push({
+      contactEmail: user.contactEmail,
+      contactPassword: user.contactPassword,
+    });
+  }
+
+  return users;
+}
+
+async function initializeCheck() {
+  allUser = await getData();
+  checkLogin();
+}
+
+document.querySelector("#LoginButton").addEventListener("click", function (event) {
+  event.preventDefault();
+  initializeCheck();
+});
+
+function checkLogin() {
+  let loginEmail = document.getElementById("inputEmail").value;
+  let loginPassword = document.getElementById("inputPassword1").value;
+
+  let user = allUser.find((user) => {
+    return user.contactEmail.includes(loginEmail) && user.contactPassword.includes(loginPassword);
+  });
+
+  if (user) {
+    window.location.assign("./index.html");
+  } else {
+    document.getElementById("wrongPassword").classList.remove("d-none");
+  }
+}
+
+document.querySelector(".formInputContainer").addEventListener("click", function (event) {
+  document.getElementById("wrongPassword").classList.add("d-none");
+});
