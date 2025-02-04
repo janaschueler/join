7ce5a0ab2abeb3dde050315ee69c8ec171510fec
Jana Schueler <janaschueler@protonmail.com>
2025-02-04 14:27:12 +0100

    up-date Login Mobile

--- scripts/login.js
+++ scripts/login.js
@@ -2,28 +2,84 @@ const BASE_URL = "https://join-ab0ac-default-rtdb.europe-west1.firebasedatabase.
 
 let allUser = { contactEmail: [], contactPassword: [] };
 
-function onloadFunc() {
+function init() {
   showLoadingMessage();
+  // setTimeout(hideLoadingBackground, 3000); // Nach 3 Sekunden ausblenden
 }
 
 function showLoadingMessage() {
+  const loadingMessageMobile = document.getElementById("loadingMessageMobile");
   const loadingMessage = document.getElementById("loadingMessage");
-  loadingMessage.style.display = "flex";
-  setTimeout(() => {
-    loadingMessage.style.width = "100px";
-    loadingMessage.style.height = "50px";
-    loadingMessage.style.transform = "scale(0.375) translate(100px, -150px)";
-    loadingMessage.style.top = "133px";
-    loadingMessage.style.left = "68px";
-  }, 1000);
   const loginContainer = document.getElementById("loginContainer");
   const footer = document.getElementById("footer");
   const header = document.getElementById("header");
+  const mobileNav = document.getElementById("mobileNav");
+  const mobileLogoChange = document.getElementById("mobileLogoChange");
+  const mobileLogoChangeDark = document.getElementById("mobileLogoChangeDark");
+
+  if (window.innerWidth > 480) {
+    setTimeout(() => {
+      document.getElementById("loadingMessage").classList.add("show");
+    }, 100);
+    setTimeout(() => {
+      loadingMessage.style.display = "flex";
+      loadingMessage.style.width = "100px";
+      loadingMessage.style.height = "50px";
+      loadingMessage.style.transform = "scale(0.375) translate(100px, -150px)";
+      loadingMessage.style.top = "133px";
+      loadingMessage.style.left = "68px";
+    }, 1000);
+
+    setTimeout(() => {
+      loginContainer.style.display = "flex";
+      footer.style.display = "block";
+      header.style.display = "flex";
+
+      loginContainer.classList.add("show");
+      footer.classList.add("show");
+      header.classList.add("show");
+    }, 1500);
+
+    return;
+  }
+
   setTimeout(() => {
+    document.getElementById("loadingMessageMobile").classList.add("show");
+  }, 100);
+  setTimeout(() => {
+    loadingMessageMobile.style.display = "flex";
+    loadingMessageMobile.style.width = "200px";
+    loadingMessageMobile.style.height = "50px";
+    loadingMessageMobile.style.transform = "scale(0.375) translate(100px, -150px)";
+    loadingMessageMobile.style.top = "64px";
+    loadingMessageMobile.style.left = "-80px";
+  }, 1000);
+
+  setTimeout(() => {
+    mobileLogoChange.style.display = "none";
+    mobileLogoChangeDark.style.display = "block";
+    mobileLogoChangeDark.style.opacity = "1";
+    mobileLogoChangeDark.style.visibility = "visible";
+  }, 1500);
+
+  setTimeout(() => {
+    document.body.style.setProperty("background", "rgb(246, 247, 248)", "important");
     loginContainer.style.display = "flex";
     footer.style.display = "block";
-    header.style.display = "flex";
+    mobileNav.style.display = "flex";
+    loginContainer.classList.add("show");
+    footer.classList.add("show");
+    mobileNav.classList.add("show");
   }, 1500);
+}
+
+function hideLoadingBackground() {
+  const loadingBackground = document.getElementById("loadingBackground");
+  loadingBackground.style.opacity = "0";
+
+  setTimeout(() => {
+    loadingBackground.style.display = "none";
+  }, 1000);
 }
 
 async function getData(path = "") {
