af1f4d4fdc06933feb5c5d32af248e02d63f8eec
Jana Schueler <janaschueler@protonmail.com>
2025-03-08 11:55:14 +0100

    clean Code

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/**
- * This function is used to greet someone on the comand line
- *
- * @param {string} name - this is the dame of the person that you want ot greet
- */
-
 function greet(name) {
   console("hallo" + name);
 }
